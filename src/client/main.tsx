89a7f8e52949bd93ed3f8c04ce0ec6cf3c929829
yuki_yano <yuki.ycino@gmail.com>
2025-08-03 10:34:13 +0900

    chore: 開発環境専用ページ（DevR2Images、OGPDebugger）を削除
    - /dev/r2-images と /dev/ogp-debugger のルートを削除
    - 不要になった開発用コンポーネントを削除

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -6,8 +6,6 @@ import { ScreenshotProvider } from "./contexts/ScreenshotContext"
 import App from "./App"
 import Replay from "./pages/Replay"
 import DeckImageReplacer from "./pages/DeckImageReplacer"
-import { DevR2Images } from "./pages/DevR2Images"
-import { OGPDebugger } from "./pages/OGPDebugger"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
@@ -19,12 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
             <Route path="/" element={<App />} />
             <Route path="/replay/:id" element={<Replay />} />
             <Route path="/deck-image-replacer" element={<DeckImageReplacer />} />
-            {import.meta.env.DEV && (
-              <>
-                <Route path="/dev/r2-images" element={<DevR2Images />} />
-                <Route path="/dev/ogp-debugger" element={<OGPDebugger />} />
-              </>
-            )}
           </Routes>
         </BrowserRouter>
       </ScreenshotProvider>
